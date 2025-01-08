8ed9276f252f24db855eb62db94b15bdf4fa1789
shubhamsrnm <shubhamsrmn@gmail.com>
2025-01-08 15:40:17 +0530

    default window width adjusted

--- src/utils/hooks/useWindowWidth.ts
+++ src/utils/hooks/useWindowWidth.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const useWindowWidth = () => {
   const [windowWidth, setWindowWidth] = useState(
-    typeof window !== "undefined" ? window.innerWidth : 0
+    typeof window !== "undefined" ? window.innerWidth : 1280
   );
 
   useEffect(() => {
